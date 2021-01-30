dc0a0e543c4ab4a9a932a113b28d7dd3351b00ab
niksh <NikolayShell612@gmail.com>
2021-01-30 12:37:26 +0300

    Initial Commit

---
+++ pryaniki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pryaniki
+//
+//  Created by Nikolay Shelepov on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
